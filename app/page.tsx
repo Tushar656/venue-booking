f8185186b26580e1e58cfe89855a2d051a85599e
Tushar656 <ui20cs69@iiitsurat.ac.in>
2026-02-09 01:40:52 +0530

    remove tw handler

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,6 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center mx-auto px-4">
-          <Link
-            href="https://twitter.com/shadcn"
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
-            target="_blank"
-          >
-            Follow along on Twitter
-          </Link>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight text-foreground">
             A minimalist venue booking for modern teams.
           </h1>
